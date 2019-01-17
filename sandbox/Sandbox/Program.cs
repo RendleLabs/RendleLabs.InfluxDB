594412f875cc79129e48659f60f1c75016b93493
Mark Rendle <mark@rendlelabs.com>
2019-01-17 12:19:02 +0000

    Add sandbox

---
+++ sandbox/Sandbox/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Sandbox
+{
+    class Program
+    {
+        private static readonly DiagnosticSource _diagnostics = new DiagnosticListener(typeof(Program).FullName);
+
+        static void Main(string[] args)
+        {
+            if (_diagnostics.IsEnabled("Main"))
+            {
+                _diagnostics.Write("Main", new { arg_count = args.Length });
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
