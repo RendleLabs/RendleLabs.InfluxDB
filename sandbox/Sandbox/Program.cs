cecc2f75cbd4f7bada1d39c3f96e065062182a79
Mark Rendle <mark@rendlelabs.com>
2019-01-17 15:52:47 +0000

    Activity

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Sandbox
 {
     class Program
     {
-        private static readonly DiagnosticSource _diagnostics = new DiagnosticListener(typeof(Program).FullName);
+        private static readonly DiagnosticSource _diagnostics 
+            = new DiagnosticListener(typeof(Program).FullName);
 
         static void Main(string[] args)
         {
@@ -13,8 +15,29 @@ namespace Sandbox
             {
                 _diagnostics.Write("Main", new { arg_count = args.Length });
             }
-            
-            Console.WriteLine("Hello World!");
+        }
+
+        static async Task DoAThing(int id)
+        {
+            if (_diagnostics.IsEnabled("Things"))
+            {
+                var activity = new Activity("DoThing");
+                var args = new { id };
+                _diagnostics.StartActivity(activity, args);
+                try
+                {
+                    await DoIt();
+                }
+                finally
+                {
+                    _diagnostics.StopActivity(activity, args);
+                }
+            }
+        }
+
+        static Task DoIt()
+        {
+            return Task.CompletedTask;
         }
     }
 
